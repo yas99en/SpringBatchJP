e3068093982184ef67fff0b05125574e51a64bf6
quive <quive@torino>
2025-03-06 22:46:44 +0900

    spring batch5対応

--- chapter03-HelloWorld/BatchHelloWorldChunk/src/main/java/com/example/demo/config/BatchConfig.java
+++ chapter03-HelloWorld/BatchHelloWorldChunk/src/main/java/com/example/demo/config/BatchConfig.java
@@ -3,27 +3,27 @@ package com.example.demo.config;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 @EnableBatchProcessing
 public class BatchConfig {
 
-    /** JobBuilderのFactoryクラス */
     @Autowired
-    private JobBuilderFactory jobBuilderFactory;
+    private JobRepository jobRepository;
 
-    /** StepBuilderのFactoryクラス */
     @Autowired
-    private StepBuilderFactory stepBuilderFactory;
+    private PlatformTransactionManager transactionManager;
 
     /** HelloReader */
     @Autowired
@@ -40,8 +40,8 @@ public class BatchConfig {
     /** ChunkのStepを生成 */
     @Bean
     public Step chunkStep() {
-        return stepBuilderFactory.get("HelloChunkStep") // Builderの取得
-            .<String, String>chunk(1) // チャンクの設定
+        return new StepBuilder("HelloChunkStep", jobRepository) // Builderの取得
+            .<String, String>chunk(1, transactionManager) // チャンクの設定
             .reader(reader) // readerセット
             .processor(processor) // processorセット
             .writer(writer) // writerセット
@@ -51,7 +51,7 @@ public class BatchConfig {
     /** Jobを生成 */
     @Bean
     public Job chunkJob() throws Exception {
-        return jobBuilderFactory.get("HelloWorldChunkJob") // Builderの取得
+        return new JobBuilder("HelloWorldChunkJob", jobRepository) // Builderの取得
             .incrementer(new RunIdIncrementer()) // IDのインクリメント
             .start(chunkStep()) // 最初のStep
             .build(); // Jobの生成

a70b6dc1e64acb1dd41f6623d51275973fdee87b
yas99en <yas99en@gmail.com>
2025-03-06 23:03:18 +0900

    spring batch5

--- chapter03-HelloWorld/BatchHelloWorldChunk/src/main/java/com/example/demo/config/BatchConfig.java
+++ chapter03-HelloWorld/BatchHelloWorldChunk/src/main/java/com/example/demo/config/BatchConfig.java
@@ -2,7 +2,6 @@ package com.example.demo.config;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
@@ -16,7 +15,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
-@EnableBatchProcessing
 public class BatchConfig {
 
     @Autowired

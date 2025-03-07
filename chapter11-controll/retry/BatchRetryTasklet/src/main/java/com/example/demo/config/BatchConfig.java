8eb8b601dfac408d1efce7ffed3bb5f0b838a075
yas99en <yas99en@gmail.com>
2025-03-07 17:27:26 +0900

    spring batch5

--- chapter11-controll/retry/BatchRetryTasklet/src/main/java/com/example/demo/config/BatchConfig.java
+++ chapter11-controll/retry/BatchRetryTasklet/src/main/java/com/example/demo/config/BatchConfig.java
@@ -2,26 +2,24 @@ package com.example.demo.config;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
-@EnableBatchProcessing
 public class BatchConfig {
 
-    /** StepBuilderのFactoryクラス. */
     @Autowired
-    private StepBuilderFactory stepBuilderFactory;
+    protected JobRepository jobRepository;
 
-    /** JobBuilderのFactoryクラス. */
     @Autowired
-    private JobBuilderFactory jobBuilderFactory;
+    protected PlatformTransactionManager transactionManager;
 
     @Autowired
     private Tasklet retryTasklet;
@@ -29,15 +27,15 @@ public class BatchConfig {
     /** TaskletのStepを生成 */
     @Bean
     public Step retryTaskletStep() {
-        return stepBuilderFactory.get("RetryTaskletStep")
-            .tasklet(retryTasklet)
+        return new StepBuilder("RetryTaskletStep", jobRepository)
+            .tasklet(retryTasklet, transactionManager)
             .build();
     }
 
     /** TaskletのJobを生成 */
     @Bean
     public Job retryTaskletJob() throws Exception {
-        return jobBuilderFactory.get("RetryTaskletJob")
+        return new JobBuilder("RetryTaskletJob", jobRepository)
             .incrementer(new RunIdIncrementer())
             .start(retryTaskletStep())
             .build();

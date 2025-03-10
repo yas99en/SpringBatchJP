cef29fd51790fdc7a03879aa4c5c897849df045e
yas99en <yas99en@gmail.com>
2025-03-10 16:05:22 +0900

    spring batch5

--- chapter14-test/BatchTestTasklet/src/main/java/com/example/demo/config/BatchConfig.java
+++ chapter14-test/BatchTestTasklet/src/main/java/com/example/demo/config/BatchConfig.java
@@ -2,25 +2,25 @@ package com.example.demo.config;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
-@EnableBatchProcessing
 public class BatchConfig {
 
     @Autowired
-    private JobBuilderFactory jobBuilderFactory;
+    private JobRepository jobRepository;
 
     @Autowired
-    private StepBuilderFactory stepBuilderFactory;
+    private PlatformTransactionManager transactionManager;
 
     @Autowired
     @Qualifier("Tasklet1")
@@ -32,21 +32,21 @@ public class BatchConfig {
 
     @Bean
     public Step testStep1() {
-        return stepBuilderFactory.get("Step1")
-            .tasklet(tasklet1)
+        return new StepBuilder("Step1", jobRepository)
+            .tasklet(tasklet1, transactionManager)
             .build();
     }
 
     @Bean
     public Step testStep2() {
-        return stepBuilderFactory.get("Step2")
-            .tasklet(tasklet2)
+        return new StepBuilder("Step2", jobRepository)
+            .tasklet(tasklet2, transactionManager)
             .build();
     }
 
     @Bean
     public Job testTaskletJob() {
-        return jobBuilderFactory.get("TaskletJob")
+        return new JobBuilder("TaskletJob", jobRepository)
             .incrementer(new RunIdIncrementer())
             .start(testStep1())
             .next(testStep2())
